103907a840eef90ec0601ab8d31af1371dd00d5c
FelipeM1805 <miguel.felipe@academico.ifpb.edu.br>
2024-12-06 14:45:53 -0300

    segundo commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 import flags from './model/flags.js';
+
+const main = document.querySelector('main');
+
+const flagsHtml = flags.map(createFlagView).join('');
+
+main.insertAdjacentHTML('beforeend', flagsHtml);
+
+function createFlagView(flag) {
+  return `<div class="flag col-2 my-2 text-center">
+    <img src="${flag.image}" alt="${flag.name}">
+    <p>${flag.name}</p>
+  </div>`;
+}
